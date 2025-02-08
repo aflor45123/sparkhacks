db43b4e5a30d768947d4f394e876cc155ccb4129
aflor45123 <aflor45@uic.edu>
2025-02-08 10:55:38 -0600

    Questionaire2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <stdlib.h>
-#include <iostream> 
+#include <iostream>
+#include <string> 
+#include <vector>
 #include "creature.h"
 
 using namespace std; 
@@ -9,16 +11,27 @@ invasives[0] = Creature("Queen Anne's Lace", 100, "Midwest", "A member of the ca
 invasives[1] = Creature("Tomato Worm", 100, "Midwest", "A fleshy blue worm frequently found in gardens.");
 
 void questionaire() {
-    cout << "Do you garden or farm?" << endl;
+    vector<string> questions = {"Do you garden or farm?", "Do you spend time by the water?", 
+        "Do you spend time by the forest?", "Are you a morning person or a night person?",
+        "Are you okay with touching/killing bugs?", "Are you okay with touching/kill fish?"};
+    vector<char> responses;
 
-    cout << "Do you spend time by the water?" << endl;
 
+    for (int i = 0; i < questions.size(); ++i) {
+        char answer;
+
+        cout << questions.at(i) << endl;
+        cout << "Enter [y/n]: " << endl;
+        cin >> answer;
+
+        responses.push_back(answer);
+    }
 }
 
 int main(){
     cout << "Welcome, I'm Andromeda, I will assist you in choosing ";
     cout << "the invasive species that you would like to exterminate :)" << endl;
     
-    //cout << questionaire() << endl;
+    questionaire();
     return 0;
 }
