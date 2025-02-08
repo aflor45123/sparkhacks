9ab5c1c981659531ebe7b66b2210a56786b1fd9a
root <root@Andseason1>
2025-02-07 22:54:09 -0600

    Added AI starter messages

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 #include <iostream> 
 using namespace std; 
 
+void questionaire() {
+    cout << "Do you garden or farm?" << endl;
+
+    cout << "Do you spend time by the water?" << endl;
+
+}
+
 int main(){
-    cout << "this works!!!"<< endl;
+    cout << "Welcome, I'm Andromeda, I will assist you in choosing ";
+    cout << "the invasive species that you would like to exterminate :)" << endl;
+    
+    //cout << questionaire() << endl;
     return 0;
 }
